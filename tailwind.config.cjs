460179aef9452f2f595e0cb5afb3c7bb2d35935c
Bruce <recall4056@gmail.com>
2022-09-25 00:33:09 +0800

    Add tailwind.

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+/* eslint-env node */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: ["class"],
+};
