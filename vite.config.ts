f69cd7477af02101408db5104dd506a0ad4b271e
Bruce <recall4056@gmail.com>
2022-09-25 00:18:35 +0800

    Update.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { fileURLToPath, URL } from "node:url";
+// import { fileURLToPath, URL } from "node:url";
+import path from "path";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -9,7 +10,10 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      // "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@views": path.resolve(__dirname, "src/views"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
 });
