0ab45865589501f598b2fba4a8ec12cf6dff3dba
Bruce <recall4056@gmail.com>
2022-09-29 00:08:43 +0800

    Add element UI.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import ElementPlus from "element-plus";
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./i18n";
@@ -9,6 +10,7 @@ const app = createApp(App);
 
 app.use(router);
 app.use(i18n);
+app.use(ElementPlus, { size: "small", zIndex: 3000 });
 
 // TODO - Move to environment variables
 app.config.globalProperties.version = "1.0.0";
