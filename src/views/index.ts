47847bcfa98f3d88a5f6dcfc02c3995ae3e6c9ec
Bruce Song <recall4056@gmail.com>
2022-10-02 22:37:19 +0800

    Update config.

--- src/views/index.ts
+++
@@ -1,4 +0,0 @@
-import Home from "./Home/HomePage.vue";
-import ResumeEditor from "./ResumeEditor/ResumeEditorLayout.vue";
-
-export { Home, ResumeEditor };
--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,9 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "src/components"),
       "@assets": path.resolve(__dirname, "src/assets")
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    reportCompressedSize: false
   }
 });
