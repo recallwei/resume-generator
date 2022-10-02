a40b2d6cd51ca95143f5d1a78c02b46a31d214f4
Bruce Song <recall4056@gmail.com>
2022-10-02 17:16:29 +0800

    Update.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: { home: "url('/src/assets/background/home.jpg')" },
+      backgroundImage: { home: "url('/background/home.jpg')" },
       colors: {
         emerald: {
           DEFAULT: "#50c878",
