1f1b6ffb774ba1ce050f2decadcba44e30688b2f
Bruce Song <recall4056@gmail.com>
2022-10-02 12:09:38 +0800

    Update.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,5 +22,25 @@ module.exports = {
     }
   },
   plugins: [require("daisyui")],
-  darkMode: ["class"]
+  darkMode: ["class"],
+  daisyui: {
+    styled: true,
+    themes: [
+      {
+        default: {
+          primary: "#16a34a",
+          secondary: "#f6d860",
+          accent: "#50c878",
+          neutral: "#3d4451",
+          "base-100": "#ffffff"
+        }
+      }
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "daisy-",
+    darkTheme: "dark"
+  }
 };
