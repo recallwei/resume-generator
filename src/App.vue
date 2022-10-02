df14e9b000373b9292ca35b17c3dc9cf878148de
Bruce Song <recall4056@gmail.com>
2022-10-02 12:56:57 +0800

    Update.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import { ElConfigProvider } from "element-plus";
+import zhCn from "element-plus/lib/locale/lang/zh-cn";
+
+const locale = zhCn;
 </script>
 
 <template>
-  <RouterView />
+  <el-config-provider :locale="locale">
+    <RouterView />
+  </el-config-provider>
 </template>
