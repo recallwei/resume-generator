3c1e07adf3cd621a2d79bffe1b357f61bb9d364e
Bruce Song <recall4056@gmail.com>
2022-10-04 23:09:41 +0800

    Update.

--- src/components/MarkdownEditor.vue
+++ src/components/MarkdownEditor.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import * as monaco from "monaco-editor";
+
+onMounted(() => {
+  const editorInstance = monaco.editor.create(
+    document.getElementById("markdown-editor") as HTMLElement,
+    {
+      value: "test.md",
+      language: "markdown",
+      automaticLayout: true,
+      scrollBeyondLastLine: false,
+      tabSize: 2
+    }
+  );
+
+  editorInstance.onDidChangeModelContent(() => {
+    const value = editorInstance.getValue();
+    console.log(value);
+  });
+});
+</script>
 
 <template>
-  <div id="codeEditBox" class="bg-blue-200 h-full w-1/2" />
+  <div id="markdown-editor" class="h-full w-1/2" />
 </template>
