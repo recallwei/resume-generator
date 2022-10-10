ddd06525524a8fa8405c929f04f793bba94fcc28
Bruce <recall4056@gmail.com>
2022-10-10 11:40:32 +0800

    Update.

--- src/views/ResumeEditor/Main/ResumePreview.vue
+++ src/views/ResumeEditor/Main/ResumePreview.vue
@@ -34,12 +34,12 @@ const markdownRender = computed(() => {
   overflow-x: hidden;
   overflow-y: auto;
 }
-:deep(.editor) h1,
-:deep(.editor) h2,
-:deep(.editor) h3,
-:deep(.editor) h4,
-:deep(.editor) h5,
-:deep(.editor) h6 {
+.editor >>> h1,
+.editor >>> h2,
+.editor >>> h3,
+.editor >>> h4,
+.editor >>> h5,
+.editor >>> h6 {
   color: v-bind("previewSettingsStore.themeColor");
   font-weight: bolder;
   font-size: larger;
