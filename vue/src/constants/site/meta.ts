c315e0a0d95f4da34484e3fb05819d5b7fb89820
Bruce Song <recall4056@gmail.com>
2022-12-21 08:43:39 +0800

    Update.

--- vue/src/constants/site/meta.ts
+++ vue/src/constants/site/meta.ts
@@ -1,6 +1,7 @@
 import type { SiteMetaData } from "@/interfaces"
 
 const siteMetaData: SiteMetaData = {
+  appName: "Resume Generator",
   repoUrl: "https://github.com/recallwei/resume-generator"
 }
 
--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router"
 import NProgress from "nprogress"
+import { useSiteMetaData } from "@/hooks"
+
+const { appName } = useSiteMetaData()
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,22 +10,34 @@ const router = createRouter({
     {
       path: "/",
       name: "Home",
-      component: () => import("@/views/Home/index.vue")
+      component: () => import("@/views/Home/index.vue"),
+      meta: {
+        title: appName
+      }
     },
     {
       path: "/editor",
       name: "Editor",
-      component: () => import("@/views/Editor/index.vue")
+      component: () => import("@/views/Editor/index.vue"),
+      meta: {
+        title: "Editor"
+      }
     },
     {
       path: "/components",
       name: "Components",
-      component: () => import("@/views/Components/index.vue")
+      component: () => import("@/views/Components/index.vue"),
+      meta: {
+        title: "Components Test"
+      }
     },
     {
       path: "/:pathMatch(.*)*",
       name: "NotFound",
-      component: () => import("@/views/NotFound/index.vue")
+      component: () => import("@/views/NotFound/index.vue"),
+      meta: {
+        title: "Not Found"
+      }
     }
   ]
 })
@@ -31,6 +46,7 @@ const router = createRouter({
 router.beforeEach((to, from) => {
   if (to.path !== from.path) {
     NProgress.start()
+    document.title = to.meta.title === appName ? to.meta.title : `${to.meta.title} | ${appName}`
   }
 })
 router.afterEach(() => NProgress.done())
