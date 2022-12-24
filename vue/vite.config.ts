31bb58ff419691f38a112d1974e2b2e274af3aa1
Bruce Song <recall4056@gmail.com>
2022-12-24 21:27:02 +0800

    Update.

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -26,18 +26,18 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api-prefix/, "")
       }
     }
-  }
+  },
   // build: {
   //   chunkSizeWarningLimit: 5000,
-  //   reportCompressedSize: false,
+  //   reportCompressedSize: false
   // },
-  // optimizeDeps: {
-  //   include: [
-  //     "monaco-editor/esm/vs/language/json/json.worker",
-  //     "monaco-editor/esm/vs/language/css/css.worker",
-  //     "monaco-editor/esm/vs/language/html/html.worker",
-  //     "monaco-editor/esm/vs/language/typescript/ts.worker",
-  //     "monaco-editor/esm/vs/editor/editor.worker"
-  //   ]
-  // }
+  optimizeDeps: {
+    include: [
+      "monaco-editor/esm/vs/language/json/json.worker",
+      "monaco-editor/esm/vs/language/css/css.worker",
+      "monaco-editor/esm/vs/language/html/html.worker",
+      "monaco-editor/esm/vs/language/typescript/ts.worker",
+      "monaco-editor/esm/vs/editor/editor.worker"
+    ]
+  }
 })
