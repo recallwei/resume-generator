bc0aa892feb3b64d8fd09fba63ff6d570467de0c
Bruce Song <recall4056@gmail.com>
2022-12-24 22:37:36 +0800

    Update.

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,10 +1,7 @@
 // Fix the error when import the .vue file in .ts file.
-declare module "*" {
-  import Vue from "vue"
-  export default Vue
+declare module "*.vue" {
+  import { type DefineComponent } from "vue"
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
-// declare module "*.vue" {
-//   import { type DefineComponent } from "vue"
-//   const component: DefineComponent<{}, {}, any>
-//   export default component
-// }
+declare module "*"
